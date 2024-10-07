147719c1504f24e3ce7bb527630d0e5e63f59e64
Eduardo Escareno <isc_escareno@outlook.com>
2024-10-06 23:48:58 -0400

    uploading project

---
+++ listener-service/event/consumers.go
@@ -0,0 +1,26 @@
+package event
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Consumer struct {
+	conn      *amqp.Connection
+	queueName string
+}
+
+func NewConsumer(conn *amqp.Connection) (Consumer, error) {
+	consumer := Consumer{
+		conn: conn,
+	}
+
+}
+
+func (consumer *Consumer) setup() error {
+	channel, err := consumer.conn.Channel()
+	if err != nil {
+		return err
+	}
+
+	return declareExchange(channel)
+}
